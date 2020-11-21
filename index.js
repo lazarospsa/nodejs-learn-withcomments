e7a250ad0fa8fb87e2bc8bf96e9a651d5a7fd19a
Lazaros Psarokostas <lazarospsa@hotmail.com>
2020-11-21 15:55:08 +0200

    connect mongo to original routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //kanw eisagwgh ta paketa pou 9elw na xrhsimopoihsw
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const path = require('path')
 require('dotenv').config()
 //eisagagw to diko mou module
@@ -11,12 +11,6 @@ const app = express()
 //gia na servirei static files
 app.use(express.static(path.join(__dirname, 'public')))
 const port = process.env.port || 3000
-
-//---------------insert mongoose to project--------------
-let MongoClient = require('mongodb').MongoClient;
-const url = process.env.db_host;
-//---------------insert mongoose to project--------------
-
 
 //eisagw auta ta duo middlewares gia na mporw na pairnw post requests
 app.use(bodyParser.json());
@@ -31,100 +25,17 @@ app.use(logger)
 //npm i -g autocannon
 //autocannon -c 100 -d 1 -p 10 http://localhost:3000/api/students/1
 
-//post a new row to mongo db
-app.post('/trymongo', (req, res) => {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        useNewUrlParser: true
-        useUnifiedTopology: true
-        useCreateIndex: true
-        var dbo = db.db(process.env.db_name);
-        dbo.collection("customers").insertOne({
-            name: req.body.name,
-            age: req.body.age
-        }, 
-        function(err, result) {
-            if (err) throw err;
-            res.json(result);
-            db.close();
-        });
-    });
-});
-
-//gets all mongo db results that have name (like parametres)
-app.get('/trymongo/name/:name', (req, res) => {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        useNewUrlParser: true
-        useUnifiedTopology: true
-        useCreateIndex: true
-        var dbo = db.db(process.env.db_name);
-        dbo.collection("customers").find({
-            name: req.params.name
-        }).toArray(function (err, result) {
-            if (err) {
-                res.send(err);
-            } else {
-                res.json(result);
-                // res.send(JSON.stringify(result));
-            }
-        })
-    });
-});
-
-//gets all mongo db results that have that age (from params)
-app.get('/trymongo/:age', (req, res) => {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        useNewUrlParser: true
-        useUnifiedTopology: true
-        useCreateIndex: true
-        var dbo = db.db(process.env.db_name);
-        dbo.collection("customers").findOne({
-            age: req.params.age
-        }, 
-        function(err, result) {
-            if (err) throw err;
-            res.json(result);
-            db.close();
-        });
-    });
-});
-
-//gets all mongo db results
-app.get('/trymongo', (req, res) => {
-    // mongoose.connect(mongoConnectionString, {useNewUrlParser: true, useUnifiedTopology: true});
-    MongoClient.connect(url, async function(err, db) {
-    if (err) throw err;
-    useNewUrlParser: true
-    useUnifiedTopology: true
-    useCreateIndex: true
-
-    var dbo = db.db(process.env.db_name);
-    var coll = dbo.collection('customers');
-
-    coll.find({}).toArray(function (err, result) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.json(result);
-            // res.send(JSON.stringify(result));
-        }
-    })
-    })
-})
-
 //routes for web
 app.use('/',require('./routes/web'))
 //routes for api
 app.use('/api/students/',require('./routes/api/students'))
+//routes for mongo api
+app.use('/mongo/',require('./routes/api/mongo'))
 //start server
 app.listen(port, () => console.log(`Server running on port: ${port}`))
 
 //start db -> cd db && mongod --dbpath=. ----- OXI AUTO
 //brew services start mongodb-community ---- <<<< ME AUTO
-
-
 //ean vgazei 9ema trexw
 //$ sudo rm -rf /tmp/mongodb-27017.sock
 //$ sudo brew services restart mongod
